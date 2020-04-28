d4015a4776cc32260139103ed7803fa6aa6903f0
koideshogo <56552221+koideshogo@users.noreply.github.com>
2020-04-28 22:39:34 +0900

    Create_Slack_View

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,14 @@
 <template>
-  <div>
-    <nuxt />
+  <div class="app-layout">
+    <div class="sidebar">
+      <p>チャンネル一覧</p>
+      <p>general</p>
+      <p>random</p>
+      <p>random</p>
+    </div>
+    <div class="main-content">
+      <nuxt />
+    </div>
   </div>
 </template>
 
@@ -51,5 +59,27 @@ html {
 .button--grey:hover {
   color: #fff;
   background-color: #35495e;
+}
+.app-layout {
+  display: flex;
+}
+
+.sidebar {
+  width: 300px;
+  background: #4A4141;
+  height: 100vh;
+  padding: 20px;
+}
+
+.sidebar p {
+  color: #DDDDDD;
+  padding-top: 4px;
+}
+
+
+.main-content {
+  width: 100%;
+  background: #F1F1F1;
+  height: 100vh;
 }
 </style>
